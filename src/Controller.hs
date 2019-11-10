4c36837b7e64f78dda893c0cb78785815e7e9ff8
drew <drew.boardman@modaoperandi.com>
2019-11-10 12:30:32 -0600

    missing instance of some thing

--- src/Controller.hs
+++ src/Controller.hs
@@ -1,15 +1,29 @@
-{-# LANGUAGE DataKinds     #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Controller where
 
 import           Data.Text
-
-import           Data.Time                      ( UTCTime )
-import           Models                         ( LongUrl
-                                                , ShortUrl
-                                                )
+import           Models
 import           Servant.API
+import           Servant
 
-type UrlApi
-  = "minimize" :> QueryParam "longUrl" LongUrl :> Get '[JSON] [ShortUrl]
+type Api =
+  -- GET: /minimize/<longUrl> -- returns shortened URL
+   "minimize" :> QueryParam "longUrl" LongUrl :> Get '[JSON] ShortUrl
+
+
+app :: Application
+app = serve urlApi shortenerServer
+
+-- this is important boilerplate
+-- you need it to guide type inference
+urlApi :: Proxy Api
+urlApi = Proxy
+
+shortenerServer :: Server Api
+shortenerServer = minimize where
+
+  minimize :: Maybe LongUrl -> Handler ShortUrl
+  minimize = undefined
--- src/Models.hs
+++ src/Models.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE FlexibleInstances #-}
 
-module Models where
+module Models
+  ( LongUrl(..)
+  , ShortUrl(..)
+  )
+where
 
 import           Data.Aeson
 import           Data.Aeson.Types
@@ -9,6 +13,7 @@ import           GHC.Generics
 
 data LongUrl = LongUrl { url :: String } deriving (Eq, Show, Generic)
 instance ToJSON LongUrl
+instance FromJSON LongUrl
 
 data ShortUrl = ShortUrl { id :: String } deriving (Eq, Show, Generic)
 instance ToJSON ShortUrl
