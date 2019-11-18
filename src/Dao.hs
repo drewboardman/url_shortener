5d8440ff3c5fad821fc9bc7c2c4f6a5c6c010afa
drew <drew.boardman@modaoperandi.com>
2019-11-17 19:01:38 -0600

    lint

--- src/Dao.hs
+++ src/Dao.hs
@@ -22,8 +22,8 @@ insertLongUrl :: LongUrl -> IO (Maybe Int)
 insertLongUrl l = do
   conn <- open "test.db"
   let entry  = T.unpack $ longUrlText l
-  let upsert = Query $ T.pack "INSERT or IGNORE INTO urls (str) VALUES (?)"
+      upsert = Query $ T.pack "INSERT or IGNORE INTO urls (str) VALUES (?)"
+      getId  = Query $ T.pack "SELECT last_insert_rowid()"
   _ <- execute conn upsert $ Only (entry :: String)
-  let getId = Query $ T.pack "SELECT last_insert_rowid()"
   r <- query_ conn getId :: IO [RowId]
   return $ rowId <$> listToMaybe r
