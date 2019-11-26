c0fa7cf5eb158821c89c01d88fc77c6920035f2d
drew <drew.boardman@modaoperandi.com>
2019-11-26 12:53:11 -0600

    add logging and main method

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,10 @@ module Main where
 import           Controller
 import           Lib
 import           Network.Wai.Handler.Warp
+import           Network.Wai.Logger
 
 main :: IO ()
-main = run 8081 app
+main = withStdoutLogger $ \aplogger -> do
+  let settings = setPort 8081 $ setLogger aplogger defaultSettings
+  runSettings settings app
+
